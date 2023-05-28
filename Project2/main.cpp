90598f8e1610eccd9812109bb5cd1c179fbfac00
ronsider <ronsider2@gmail.com>
2023-05-28 13:39:31 +0300

    redisgn main function

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,10 +1,9 @@
-#include "adder.hpp"
 #include <iostream>
 using std::cout;
 using std::endl;
 int main()
 {
-	cout << adder_functions::add(9, 8) <<endl;
-	cout << adder_functions::add(1.2f, 3.9f);
+	cout << "hello world"  << endl;
+	cout << "hi there";
 	return 0;
 }
